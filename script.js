5f6d476dcf917f609589038b0f50f03125f703a8
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-01-09 21:05:37 +0800

    added some validation

--- script.js
+++ script.js
@@ -2,33 +2,50 @@ $(document).ready(function () {
 
     $("#res").hide();
 
+    $("#frm").validate({
+        rules:{
+
+            input1: "required",
+            radb: "required"
+
+        },
+        errorPlacement: function(error, element) {
+            if ( element.is(":radio") ) {
+                error.insertAfter( element.parent().parent());
+            }
+            else { 
+                error.insertAfter( element );
+            }
+        },
+        messages:{
+            input1: "<b>Please enter a street name",
+            radb: "<b>Please choose one"
+        }
+    })
+    
 
 
     document.getElementById("btnSubmit").addEventListener("click", function(event){
         event.preventDefault();
         event.stopImmediatePropagation();
 
-         
-        if (document.getElementById("inputText") == ""){  
-            
-            alert("Name can't be blank"); 
-            return false;   
-            
+        
+        $("#frm").valid();
+        let bool = document.querySelector('input[name="radb"]:checked').value;
+        $("#form1").hide();
+        $("#res").show();
+        Cloud();
+        return true;
         }
-        else{
-            let bool = document.querySelector('input[name="radb"]:checked').value;
-
-            console.log(bool);
-            $("#frm").hide();
-            $("#res").show();
-            Cloud();
-            return true;
-        }
-    })
+        ,false)
+    
 
     function Cloud(){
         let number = document.getElementById("inputNumber").value;
         let name = document.getElementById("inputText").value;
+        let chk = document.querySelector('input[name = "radb"]:checked').value;
+        
+        // first ajax call
         $.ajax({
             type: "GET",
             datatype: 'json',
@@ -36,20 +53,22 @@ $(document).ready(function () {
             
             url: `https://developers.onemap.sg/commonapi/search?searchVal=${number, name}&getAddrDetails=Y&returnGeom=Y`,
             success: function(result){
-                //Set result to a variable for writing
                 var TrueResult = JSON.stringify(result);
                 console.log(TrueResult);
                 let lat = (result.results[0].LATITUDE);
                 let long = (result.results[0].LONGTITUDE);
+
                 console.log("Latitude: ",lat)
                 console.log("Longitude: ", long)
+                // second ajax call
                 $.ajax({
                     type: "GET",
                     datatype: 'json',
                     contentType: "text/plain",
-                    url:`https://developers.onemap.sg/privateapi/commonsvc/revgeocode?location=${lat},${long}&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY5MzgsInVzZXJfaWQiOjY5MzgsImVtYWlsIjoiam9zaWFobG93MkBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cDpcL1wvb20yLmRmZS5vbmVtYXAuc2dcL2FwaVwvdjJcL3VzZXJcL3Nlc3Npb24iLCJpYXQiOjE2MDk4NTY0NjUsImV4cCI6MTYxMDI4ODQ2NSwibmJmIjoxNjA5ODU2NDY1LCJqdGkiOiIzNmQyZTBmN2RiNmQwMGZjNGQ1ZDdjYTYxZTJiNjcxOSJ9.ThDIaUjjGsUJe8Cq-Vp2KlRY-NmbLlResRlFeahi6Ac&buffer=100&addressType=all`,
+                    url:`https://developers.onemap.sg/privateapi/commonsvc/revgeocode?location=${lat},${long}&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY5MzgsInVzZXJfaWQiOjY5MzgsImVtYWlsIjoiam9zaWFobG93MkBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cDpcL1wvb20yLmRmZS5vbmVtYXAuc2dcL2FwaVwvdjJcL3VzZXJcL3Nlc3Npb24iLCJpYXQiOjE2MTAxMTY5NDcsImV4cCI6MTYxMDU0ODk0NywibmJmIjoxNjEwMTE2OTQ3LCJqdGkiOiJkMmM0YmI4NDlhM2JmOTYwZDM5MDdlZmY5ZTM4OTUwMiJ9.-JRB9LxQjynMOx_iGyT62DncZcAPH2VyTCONvDwQBKU&buffer=${chk}&addressType=all`,
                     success: function(result2){
-                        var TrueResult2 = JSON.stringify(result2)                    
+                        var TrueResult2 = JSON.stringify(result2)  
+                        console.log(chk)                  
                         var i;
                         for (i = 0; i < TrueResult2.length; i++) {
                             let build = result2.GeocodeInfo[i].BUILDINGNAME;
